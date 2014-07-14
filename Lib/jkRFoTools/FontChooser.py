3c7d282ed56c0b0e4ba11d61ac76d30228192058
Jens Kutilek <webmail@kutilek.de>
2014-07-14 20:58:55 +0200

    Change result list to text box, add Copy button

--- Lib/jkRFoTools/FontChooser.py
+++ Lib/jkRFoTools/FontChooser.py
@@ -1,6 +1,7 @@
 import vanilla
 from defconAppKit.windows.baseWindow import BaseWindowController
 from defconAppKit.windows.progressWindow import ProgressWindow
+from lib.scripting.codeEditor import CodeEditor
 from robofab.world import AllFonts
 
 class ProcessFonts(BaseWindowController):
@@ -9,7 +10,7 @@ class ProcessFonts(BaseWindowController):
             
             message:      The title to display in the title bar, e. g. the name of your script
             function:     A function that will be called for each selected font with the RFont as its argument
-            show_results: Boolean to indicate if your function returns a result which should be displayed in the result list
+            show_results: Boolean to indicate if your function returns a result which should be displayed in the result box
             width:        The initial width of the window (optional)
             height:       The initial height of the window (optional)
             
@@ -23,14 +24,6 @@ class ProcessFonts(BaseWindowController):
         self.function = function
         
         column_descriptions = [
-            {
-                "title": "Font",
-                "typingSensitive": True,
-                "editable": False,
-            },
-        ]
-        
-        result_column_descriptions = [
             {
                 "title": "Font",
                 "typingSensitive": True,
@@ -52,25 +45,26 @@ class ProcessFonts(BaseWindowController):
             columnDescriptions = column_descriptions,
             drawFocusRing = True,
             allowsMultipleSelection = True,
+            doubleClickCallback = self.copy_result,
+            selectionCallback = self.show_result,
         )
         
-        self.w.result_list = vanilla.List(
-            (10, 150, -10, -42),
-            [],
-            columnDescriptions = result_column_descriptions,
-            drawFocusRing = True,
-            allowsMultipleSelection = True,
-            doubleClickCallback = self.copy_result,
+        self.w.result_box = CodeEditor((10, 150, -10, -42), "", lexer="text")
+        
+        self.w.copy_button = vanilla.Button(
+            (10, -32, 110, -10),
+            "Copy results",
+            callback = self.copy_result,
         )
         
         self.w.cancel_button = vanilla.Button(
-            (-170, -32, -90, -10),
+            (-180, -32, -100, -10),
             "Cancel",
             callback = self.cancel,
         )
         
         self.w.ok_button = vanilla.Button(
-            (-80, -32, -10, -10),
+            (-90, -32, -10, -10),
             "Process",
             callback = self.ok,
         )
@@ -85,6 +79,7 @@ class ProcessFonts(BaseWindowController):
     def ok(self, sender=None):
         self.w.cancel_button.enable(False)
         self.w.ok_button.enable(False)
+        self.w.copy_button.enable(False)
         
         fonts = self.w.font_list.getSelection()
         
@@ -95,22 +90,32 @@ class ProcessFonts(BaseWindowController):
         )
         
         results = []
-        for i in fonts:
+        for i in range(len(AllFonts())):
             font = AllFonts()[i]
-            progress.update("Processing %s %s ..." % (font.info.familyName, font.info.styleName))
-            result = self.function(font)
-            results.append(
-                {
-                    "Font": "%s %s" % (font.info.familyName, font.info.styleName),
-                    "Result": result,
-                }
-            )
+            if i in fonts:
+                progress.update("Processing %s %s ..." % (font.info.familyName, font.info.styleName))
+                result = self.function(font)
+                results.append(
+                    {
+                        "Font": "%s %s" % (font.info.familyName, font.info.styleName),
+                        "Result": result,
+                    }
+                )
+            else:
+                results.append(
+                    {
+                        "Font": "%s %s" % (font.info.familyName, font.info.styleName),
+                        "Result": self.w.font_list.get()[i]["Result"],
+                    }
+                )
         progress.close()
-        self.w.result_list.set(results)
+        self.w.font_list.set(results)
+        self.w.font_list.setSelection(fonts)
         
         self.w.cancel_button.setTitle("Close")
         self.w.cancel_button.enable(True)
         self.w.ok_button.enable(True)
+        self.w.copy_button.enable(True)
     
     def copy_result(self, sender):
         from string import strip
@@ -118,16 +123,31 @@ class ProcessFonts(BaseWindowController):
         
         s = u""
         
-        results = self.w.result_list.getSelection()
+        results = self.w.font_list.getSelection()
         
         for i in results:
-            s += self.w.result_list.get()[i]["Font"] + "\n\n"
-            s += self.w.result_list.get()[i]["Result"] + "\n\n\n"
+            s += self.w.font_list.get()[i]["Font"] + "\n\n"
+            s += self.w.font_list.get()[i]["Result"] + "\n\n\n"
         
         pb = NSPasteboard.generalPasteboard()
         pb.clearContents()
-        a = NSArray.arrayWithObject_(s.strip(" "))
+        a = NSArray.arrayWithObject_(s.strip("\n"))
         pb.writeObjects_(a)
+    
+    def show_result(self, sender=None):
+        results = self.w.font_list.getSelection()
+        if len(results) > 0:
+            self.w.result_box.set(self.w.font_list.get()[results[0]]["Result"])
+            selection_empty = False
+        else:
+            self.w.result_box.set("")
+            selection_empty = True
+        if selection_empty:
+            self.w.ok_button.enable(False)
+            self.w.copy_button.enable(False)
+        else:
+            self.w.ok_button.enable(True)
+            self.w.copy_button.enable(True)
     
     def update_font_list(self):
         font_list = []
@@ -138,6 +158,7 @@ class ProcessFonts(BaseWindowController):
                         AllFonts()[i].info.familyName,
                         AllFonts()[i].info.styleName
                     ),
+                    "Result": ""
                 } for i in range(len(AllFonts()))
             ]
         )
